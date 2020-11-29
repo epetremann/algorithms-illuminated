bf3356f6f7ffde663eff82d63a0428e1ce67f72a
epetremann <epetremann@gmail.com>
2020-11-29 18:42:55 +0900

    Creation of sort project

---
+++ sort/lib/sort.ex
@@ -0,0 +1,18 @@
+defmodule Sort do
+  @moduledoc """
+  Documentation for `Sort`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sort.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
